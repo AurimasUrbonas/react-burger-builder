6c32255b03c0808d05396b0fc19b8b21eba61206
Aurimas Urbonas <66870338+AurimasUrbonas@users.noreply.github.com>
2021-01-30 12:47:48 +0200

    Update auth.js

--- main src/store/actions/auth.js
+++ main src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
       password: password,
       returnSecureToken: true
     };
-    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCYua2luFRT5yOr4Ho9Ne1qkZs9UDAEJn0';
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=googlekey';
     if(!isSignup) {
-      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCYua2luFRT5yOr4Ho9Ne1qkZs9UDAEJn0';
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=googlekey';
     }
     axios.post(url, authData)
       .then(response => {
